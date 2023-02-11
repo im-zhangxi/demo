1605b4dbeb916192b4945f7c3f8fd6b590bb86ea
zx <834688422@qq.com>
2023-02-11 17:58:19 +0800

    fix bug

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -13,6 +13,41 @@ public class TestController {
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("name", "zx");
         hashMap.put("age", "18");
+        hashMap.put("test", "test");
+
+        return hashMap;
+    }
+
+    @GetMapping("/login")
+    public HashMap<String, String> login () {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("id", "1");
+        hashMap.put("name", "zx");
+        hashMap.put("age", "18");
+        hashMap.put("login", "login");
+
+        return hashMap;
+    }
+
+    @GetMapping("/logout")
+    public HashMap<String, String> logout () {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("id", "1");
+        hashMap.put("name", "zx");
+        hashMap.put("age", "18");
+        hashMap.put("logout", "logout");
+
+        return hashMap;
+    }
+
+    @GetMapping("/xx")
+    public HashMap<String, String> xx () {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("id", "1");
+        hashMap.put("name", "zx");
+        hashMap.put("age", "18");
+        hashMap.put("xx", "xx");
+        hashMap.put("xx1", "11");
 
         return hashMap;
     }
